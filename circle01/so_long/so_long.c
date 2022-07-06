e3e9e3dd2d9bfdd5030653a9db701f49e2579d5a
Kyungmin Yoon <kyoon@c1r9s2.42seoul.kr>
2022-07-06 19:28:24 +0900

    so_long ~ing

--- circle01/so_long/so_long.c
+++ circle01/so_long/so_long.c
@@ -6,41 +6,63 @@
 /*   By: kyoon <kyoon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/27 17:15:14 by kyoon             #+#    #+#             */
-/*   Updated: 2022/06/27 20:44:16 by kyoon            ###   ########.fr       */
+/*   Updated: 2022/07/06 18:20:48 by kyoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+#include "stdio.h"
+#include "stdlib.h"
 
-typedef struct	s_data
+# define X_EVENT_KEY_PRESS		2
+# define X_EVENT_KEY_RELEASE	3
+
+# define KEY_ESC				53
+# define KEY_W					13
+# define KEY_A					0
+# define KEY_S					1
+# define KEY_D					2
+
+typedef struct	s_param
 {
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-}				t_data;
+	int	x;
+	int	y;
+}				t_param;
 
-void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+void	param_init(t_param *param)
 {
-	char *dst;
+	param->x = 5;
+	param->y = 5;
+}
 
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
 
-	*(unsigned int *)dst = color;
+int		key_press(int keycode, t_param *param)
+{
+	static int a;
+
+	if (keycode == KEY_W)
+		param->y--;
+	else if (keycode == KEY_A)
+		param->x--;
+	else if (keycode == KEY_S)
+		param->y++;
+	else if (keycode == KEY_D)
+		param->x++;
+	else if (keycode == KEY_ESC)
+		exit(0);
+	printf("x: %d, y: %d\n", param->x, param->y);
+	return (0);
 }
 
 int	main(void)
 {
 	void	*mlx;
 	void	*mlx_win;
-	t_data	img;
+	t_param	param;
 
+	param_init(&param);
 	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "test");
-	img.img = mlx_new_image(mlx, 1920, 1080);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	mlx_win = mlx_new_window(mlx, 500, 500, "test");
+	mlx_hook(mlx_win, X_EVENT_KEY_RELEASE, 0, &key_press, &param);
 	mlx_loop(mlx);
 }
